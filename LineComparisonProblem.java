c51681795e5d90f7b25c4c43c6a20f1dda50afb1
MoreKajal <kajalm3105@gmail.com>
2021-02-12 16:52:06 -0800

    [Kajal]Added:OOP concepts with Line compareTo()

--- LineComparisonProblem.java
+++ LineComparisonProblem.java
@@ -2,34 +2,41 @@ import java.util.Scanner;
 
 public class LineComparisonProblem
 {
-        public static void main(String args[]) {
-                System.out.println("Welcome to Line Comparison Program");
-		Scanner sc = new Scanner(System.in);
-                System.out.println("Enter First Line First Co-ordinates");
-                int x1 = sc.nextInt();
-                int y1 = sc.nextInt();
-                System.out.println("Enter First Line First Co-ordinates");
-                int x2 = sc.nextInt();
-                int y2 = sc.nextInt();
-                System.out.println("Enter Second Line First Co-ordinate");
-                int x3 = sc.nextInt();
-                int y3 = sc.nextInt();
-                System.out.println("Enter Second Line Second Co-ordinate");
-                int x4 = sc.nextInt();
-                int y4 = sc.nextInt();
+	private final double x1;
+	private final double y1;
+	private final double x2;
+	private final double y2;
 
-                double length_L1 = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1,2));
-                System.out.println("disatnce of Line1 : " + length_L1);
-                double length_L2 = Math.sqrt(Math.pow(x4-x3, 2) + Math.pow(y4-y3,2));
-                System.out.println("distance of Line2 :" + length_L2);
+	public LineComparisonProblem(double x1, double y1, double x2, double y2){
+		this.x1 = x1;
+		this.y1 = y1;
+      		this.x2 = x2;
+      		this.y2 = y2;
+   	}
+        
+	public double calLengthOfLine(){
+		double length = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
+     		return length;
+   	}
 
-                if (length_L1 > length_L2)
-                        System.out.println("Line1 Length is Greater than Line2");
+	public static void compareTo(double length_L1, double length_L2) {
+		System.out.println(" distance of Line1 :" + length_L1 + " \n distance of Line2 :" + length_L2);
+		if (length_L1 > length_L2)
+                      System.out.println("Line1 Length is Greater than Line2");
                 if (length_L1 < length_L2)
                         System.out.println("Line1 Length is Smaller than Line2");
                 else
                         System.out.println("Line1 Length is Equal to Line2");
 
+	}
+	
+	public static void main(String[] args){
+		System.out.println("Welcome to Line Comparison Program");
+		LineComparisonProblem length1_coor = new LineComparisonProblem(3,4,5,6);
+		LineComparisonProblem length2_coor = new LineComparisonProblem(3.2,4.1,5.5,6.8);
+		double length_L1 = length1_coor.calLengthOfLine();
+		double length_L2 = length2_coor.calLengthOfLine();
+		compareTo(length_L1, length_L2);
 	}
 }
 
